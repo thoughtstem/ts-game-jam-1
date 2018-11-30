5f9a93e44288bf4874727afc2a91239f8da3c194
srfoster <stephen@thoughtstem.com>
2018-11-30 12:20:09 -0800

    main

---
+++ main.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(provide
+ (all-from-out "scoring/score.rkt")
+ (all-from-out "jam/survival-game-jam.rkt")
+ #%module-begin)
+
+(require "scoring/score.rkt")
+(require "jam/survival-game-jam.rkt")
