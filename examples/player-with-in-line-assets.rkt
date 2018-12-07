4289a76b20319a02c59deca35fe0e88162cd9c2b
aBlender <jasonlongle@gmail.com>
2018-12-06 18:52:16 -0800

    Added custom-coin and coin-list and cleaned up examples

--- examples/player-with-in-line-assets.rkt
+++ examples/player-with-in-line-assets.rkt
@@ -1,16 +1,16 @@
 #lang ts-game-jam-1
 
-; Create a surivival game that spawns
 (survival-game
  #:bg              (custom-background)
  #:avatar          (custom-avatar #:sprite (row->sprite (random-character-row)))
  #:starvation-rate 20
  ;#:length-of-day   1000
  ;#:enemy-list      (list (enemy #:entity (zombie-entity) #:amount-in-world 5 #:night-only? #t)
- ;                        (enemy #:entity (slime-entity)  #:amount-in-world 10))
+ ;                        (enemy #:entity (slime-entity)  #:amount-in-world 10 #:damage 5))
  #:npc-list        (list (random-npc #:mode 'wander #:speed 1)
                          (random-npc #:mode 'wander #:speed 3))
- #:item-list       (list (coin-entity))
+ #:coin-list       (list (coin #:entity (copper-coin-entity) #:value 10  #:amount-in-world 10 )
+                         (coin #:entity (silver-coin-entity) #:value 50  #:amount-in-world 5  ))
  #:food-list       (list (food #:entity (carrot-entity)      #:amount-in-world 10)
                          (food #:entity (carrot-stew-entity) #:heals-by 20))
  #:crafter-list    (list (custom-crafter))
