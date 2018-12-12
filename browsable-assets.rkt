f50eac3002cdfc82107e9d5686fd89e142dc98b3
Sara Lucchini <saralucchini@Stephens-MacBook-Air-5.local>
2018-12-11 17:19:31 -0800

    adding (show food-sprites) in progress

--- browsable-assets.rkt
+++ browsable-assets.rkt
@@ -1,8 +1,9 @@
 #lang racket
 
-(require game-engine-rpg/lang/assets
+(require game-engine-rpg
          (except-in ts-racket scale-to-fit)
-         (only-in game-engine draw-entity)
+         (only-in game-engine draw-entity
+                              render)
          (only-in ts-curric-common text-with-image)
          pict
          pict/code)
@@ -10,6 +11,9 @@
 (provide show-coin-sprites)
 
 
+;TODO get rid of purple loading text
+
+;=== coin sprite list ===
 
 (define (show-coin-sprites)
 
@@ -17,9 +21,7 @@
   (define b (inset-frame (draw-entity (silver-coin-entity))))
   (define c (inset-frame (draw-entity (cat))))
   (define d (inset-frame (draw-entity (bat))))
-  ;(define e (inset-frame (draw-entity (slime)))) ;save for later
-  ;(define f (inset-frame (draw-entity (snake)))) ;save for later
-  (define g (inset-frame (draw-entity (apples))))
+  (define f (inset-frame (draw-entity (snake)))) 
 
   (inset-frame
    (vl-append
@@ -33,10 +35,6 @@
                 (list b
                       (hint
                        (code silver-coin-sprite))))
-    (code+hints g #:settings hints-on-right
-                (list g
-                      (hint
-                       (code apples-sprite))))
     (code+hints c #:settings hints-on-right
                 (list c
                       (hint
@@ -49,14 +47,54 @@
                 (list d
                       (hint
                        (code bat-sprite))))
-    #;(code+hints e #:settings hints-on-right
-                (list e
-                      (hint
-                       (code slime-sprite))))
-    #;(code+hints f #:settings hints-on-right
+    (code+hints f #:settings hints-on-right
                 (list f
                       (hint
                        (code snake-sprite)))))))
 
+;=== food sprite list ====
+(define (show-food-sprites)
+
+  (define a (inset-frame (draw-entity (slime)))) 
+  (define b (inset-frame (draw-entity (apples))))
+  (define c (inset-frame (draw-entity (carrot-entity))))
+  (define d (inset-frame (render toasted-marshmallow-sprite)))
+  (define e (inset-frame (render fish-sprite)))
+  ;add cherry
+  ;add steak
+
+  (inset-frame
+   (vl-append
+    (scale (text "Try these food sprites!") 1.5)
+    (ghost (rectangle 10 10))
+    (code+hints b #:settings hints-on-right
+                (list b
+                      (hint
+                       (code apples-sprite))))
+    (code+hints c #:settings hints-on-right
+                (list c
+                      (hint
+                       (code carrot-sprite))))
+    (code+hints d #:settings hints-on-right
+                (list d
+                      (hint
+                       (code toasted-marshmallow-sprite))))
+    (code+hints e #:settings hints-on-right
+                (list e
+                      (hint
+                       (vl-append
+                        (code fish-sprite)
+                        (text "Also try:")
+                        (code cooked-fish-sprite)))))
+    #;(code+hints e #:settings hints-on-right
+                (list e
+                      (hint
+                       (code slime-sprite))))
+    (code+hints a #:settings hints-on-right
+                (list a
+                      (hint
+                       (code slime-sprite)))))))
+
 (module+ test
-  (show-coin-sprites))
+  (show-coin-sprites)
+  (show-food-sprites))
