991206affaf05a30f97d89a360868208d7f2b2eb
srfoster <stephen@thoughtstem.com>
2018-12-13 09:59:13 -0800

    adding game-engine-rpg as dep

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
 
 (define deps '(
   "https://github.com/thoughtstem/game-engine.git"
-  ;"https://github.com/thoughtstem/game-engine-rpg.git"
+  "https://github.com/thoughtstem/game-engine-rpg.git"
   "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common"
   ))
 
